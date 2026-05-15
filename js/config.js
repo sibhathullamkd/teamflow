5d9f491662c2a0f5eb4efa78855518414a51fcf4
Mhmd Sibhathulla <sibhathullamkd@gmail.com>
2026-05-15 08:42:47 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var SUPABASE_URL = 'https://tnxbfkcijuilzdbulamd.supabase.co';
 var SUPABASE_ANON_KEY = 'sb_publishable_SOG1ptoVvZZR3uGeLs4WPw_4Y0F2val';
 var ADMIN_USERNAME = 'admin';
-var ADMIN_PASSWORD = 'fhzadmin2026;
+var ADMIN_PASSWORD = 'fhzadmin2026';
 
 if (!window._supabaseClient) {
   window._supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
